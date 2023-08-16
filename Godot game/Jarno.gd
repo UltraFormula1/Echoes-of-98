c1ca6cc621abcb4ee6b89fa26383944025e86afc
UltraFormula1 <concas44@kingshigh.school.nz>
2023-08-16 21:22:07 +1200

    Day 10.5
    Made edits at home

--- Godot game/Jarno.gd
+++ Godot game/Jarno.gd
@@ -1,8 +1,11 @@
 extends CharacterBody3D
 
-@export var mouse_sensitivity = 0.02
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var mouse_sensitivity = 0.01
+var SPEED = 7.0
+@export var run_speed = 13
+@export var walk_speed = 7
+@export var FRICTION = 1
+const JUMP_VELOCITY = 6.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -10,7 +13,7 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-func _unhandeled_input(event):
+func _unhandled_input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(-event.relative.x * mouse_sensitivity)
 		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
@@ -24,16 +27,21 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+	
+	if Input.is_action_pressed("run"):
+		SPEED = run_speed
+	else:
+		SPEED = walk_speed
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = move_toward(velocity.x, direction.x * SPEED,FRICTION)
+		velocity.z = move_toward(velocity.z, direction.z * SPEED,FRICTION)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, FRICTION)
+		velocity.z = move_toward(velocity.z, 0, FRICTION)
 
 	move_and_slide()
