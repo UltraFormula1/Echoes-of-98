922803d3c77e8730483cc7e516cc9d2d447d4f57
UltraFormula1 <100097049+UltraFormula1@users.noreply.github.com>
2023-08-28 15:02:52 +1200

    Day 19
    Final day prior to due date

--- Godot game/Jarno.gd
+++ Godot game/Jarno.gd
@@ -7,6 +7,12 @@ var SPEED = 7.0
 @export var FRICTION = 1
 const JUMP_VELOCITY = 6.0
 
+@onready var ray = $Camera3D/RayCast3D
+@onready var interaction_notifier = $Control/InteractionNotifier
+@onready var collection_tracker = $Control/MarginContainer/CollectionTracker
+@onready var paintings_collected = 0
+
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -19,8 +25,19 @@ func _unhandled_input(event):
 		$Camera3D.rotate_x(-event.relative.y * mouse_sensitivity)
 		$Camera3D.rotation.x = clamp($Camera3D.rotation.x, -1.2,1.2)
 
+func check_ray_hit():
+	if ray.is_colliding():
+		if ray.get_collider().is_in_group("Pickup"):
+			interaction_notifier.visible = true
+		if Input.is_action_just_pressed("Use"):
+			ray.get_collider().queue_free()
+			paintings_collected +=1
+			collection_tracker.text = "Painting's = " + str(paintings_collected)
+		else:
+			interaction_notifier.visible = false
+
 func _physics_process(delta):
-	# Add the gravity.
+	check_ray_hit()
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 
@@ -38,6 +55,7 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		$Walk.play()
 		velocity.x = move_toward(velocity.x, direction.x * SPEED,FRICTION)
 		velocity.z = move_toward(velocity.z, direction.z * SPEED,FRICTION)
 	else:
