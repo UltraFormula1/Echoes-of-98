5eec693e7e1ff8f44016848205be9b28ed5f380b
UltraFormula1 <concas44@kingshigh.school.nz>
2023-09-01 21:24:27 +1200

    Day 22.1
    Final

--- Godot game/Jarno.gd
+++ Godot game/Jarno.gd
@@ -58,7 +58,6 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		$Walk.play()
 		velocity.x = move_toward(velocity.x, direction.x * SPEED,FRICTION)
 		velocity.z = move_toward(velocity.z, direction.z * SPEED,FRICTION)
 	else:
